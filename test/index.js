95e3bf78f6f721eaece34be059e6bd91d5128550
xiaojue <designsor@gmail.com>
2018-03-06 11:14:47 +0800

    增加测试部分

---
+++ test/index.js
@@ -0,0 +1,1 @@
+import './vue2wx-template';
---
+++ test/vue2wx-template.js
@@ -0,0 +1,16 @@
+import assert from 'assert';
+import vuewx from '../src/view/index';
+
+describe('vue2wx-template', function() {
+  describe('#标签', function() {
+			
+  });
+  describe('#指令', function() {
+  });
+  describe('#class,style', function() {
+  });
+  describe('#事件', function() {
+  });
+  describe('#mustache', function() {
+  });
+});
